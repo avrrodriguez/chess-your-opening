8e295212fcdaa4217b2aeffaf48422b39a520c83
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-08 16:34:18 -0500

    Adds jb gem and changes how the openings controller renders info so that it uses custom render from jb views file

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -2,19 +2,19 @@ class OpeningsController < ApplicationController
   before_action :authenticate_admin, except: [:index, :show]
 
   def index
-    openings = Opening.all.order(:id)
+    @openings = Opening.all.order(:id)
 
-    render json: openings.as_json
+    render template: "openings/index"
   end
 
   def show
-    opening = Opening.find_by(id: params["id"])
+    @opening = Opening.find_by(id: params["id"])
 
-    render json: opening.as_json
+    render template: "openings/show"
   end
 
   def create
-    opening = Opening.new(
+    @opening = Opening.new(
       name: params["name"],
       image_url: params["image_url"],
       description: params["description"],
@@ -22,31 +22,31 @@ class OpeningsController < ApplicationController
       variation: params["variation"],
     )
 
-    if opening.save
-      render json: opening.as_json
+    if @opening.save
+      render template: "openings/show"
     else
-      render json: { "errors": opening.errors.full_messages }, status: :unprocessable_entity
+      render json: { "errors": @opening.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def update
-    opening = Opening.find_by(id: params["id"])
-    opening.name = params["name"] || opening.name
-    opening.image_url = params["image_url"] || opening.image_url
-    opening.description = params["description"] || opening.description
-    opening.difficulty = params["difficulty"] || opening.difficulty
-    opening.variation = params["variation"] || opening.variation
+    @opening = Opening.find_by(id: params["id"])
+    @opening.name = params["name"] || @opening.name
+    @opening.image_url = params["image_url"] || @opening.image_url
+    @opening.description = params["description"] || @opening.description
+    @opening.difficulty = params["difficulty"] || @opening.difficulty
+    @opening.variation = params["variation"] || @opening.variation
 
-    if opening.save
-      render json: opening.as_json
+    if @opening.save
+      render json: @opening.as_json
     else
-      render json: { "errors": errors.opening.full_messages }, status: :unprocessable_entity
+      render json: { "errors": @opening.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    opening = Opening.find_by(id: params["id"])
-    opening.destroy
+    @opening = Opening.find_by(id: params["id"])
+    @opening.destroy
 
     render json: { "message": "d.. d.. destroyed" }
   end
