8ae3bdb45c9d578025e43c693ea93c8d9099998a
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-11 12:50:16 -0500

    Adds extra information of studies in show index jb of studies, changes studies controller to render show json jb template

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -7,41 +7,41 @@ class StudiesController < ApplicationController
   end
 
   def show
-    study = Study.find_by(id: params["id"])
+    @study = Study.find_by(id: params["id"])
 
-    render json: study.as_json
+    render template: "studies/show"
   end
 
   def create
-    study = Study.new(
+    @study = Study.new(
       user_id: current_user.id,
       opening_id: params["opening_id"],
       notes: params["notes"],
       public: params["public"],
     )
-    if study.save
-      render json: study.as_json
+    if @study.save
+      render template: "studies/show"
     else
-      render json: { "errors": study.errors.full_messages }, status: :unprocessable_entity
+      render json: { "errors": @study.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def update
-    study = Study.find_by(id: params["id"])
-    study.opening_id = params["opening_id"] || study.opening_id
-    study.notes = params["notes"] || study.notes
-    study.public = params["public"] || study.public
+    @study = Study.find_by(id: params["id"])
+    @study.opening_id = params["opening_id"] || @study.opening_id
+    @study.notes = params["notes"] || @study.notes
+    @study.public = params["public"] || @study.public
 
-    if study.save
-      render json: study.as_json
+    if @study.save
+      render template: "studies/show"
     else
-      render json: { "errors": study.errors.full_messages }, status: :unprocessable_entity
+      render json: { "errors": @study.errors.full_messages }, status: :unprocessable_entity
     end
   end
 
   def destroy
-    study = Study.find_by(id: params["id"])
-    study.destroy
+    @study = Study.find_by(id: params["id"])
+    @study.destroy
     render json: { "message": "destroyed successfully" }
   end
 end
