bd485c1119a16089400b0046560772bd2fd465ba
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-11-28 15:36:02 -0500

    Adds posts create method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,13 @@ class PostsController < ApplicationController
     @posts = Post.all
     render template: "posts/show"
   end
+
+  def create
+    @posts = Post.new(
+      title: params["title"],
+      opening_id: params["opening_id"],
+      common_position_id: params["common_position_id"],
+      image: params["image"],
+    )
+  end
 end
