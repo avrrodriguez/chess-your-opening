e765388c8fe810c8259322e7990d3b54b6a50ecf
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-12-12 12:56:33 -0500

    Adds rspec test for opening show action

--- spec/requests/openings_spec.rb
+++ spec/requests/openings_spec.rb
@@ -43,4 +43,35 @@ RSpec.describe "Openings", type: :request do
       expect(openings.length).to eq(4)
     end
   end
+
+  describe "GET /openings/:id" do
+    it "returns a 200 http status for show action" do
+      opening = Opening.create!(
+        name: "Queen's Gambit",
+        description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
+        difficulty: "Beginner",
+        variation: "1. d4 d5 2. c4",
+      )
+      get "/openings/#{opening.id}.json"
+
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns an array of a post" do
+      opening = Opening.create!(
+        name: "Queen's Gambit",
+        description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
+        difficulty: "Beginner",
+        variation: "1. d4 d5 2. c4",
+      )
+
+      get "/openings/#{opening.id}.json"
+      opening = JSON.parse(response.body)
+
+      expect(opening["name"]).to eq("Queen's Gambit")
+      expect(opening["description"]).to eq("White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.")
+      expect(opening["difficulty"]).to eq("Beginner")
+      expect(opening["variation"]).to eq("1. d4 d5 2. c4")
+    end
+  end
 end
