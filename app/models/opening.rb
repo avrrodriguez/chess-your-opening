5480a25303547045c07f02268f9186525d861214
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-12-12 13:42:22 -0500

    Adds validation to opening model for name, description, difficulty, and variation. Adds tests that test opening authorization and authentication

--- app/models/opening.rb
+++ app/models/opening.rb
@@ -4,4 +4,9 @@ class Opening < ApplicationRecord
   has_many :common_positions
   has_many :users, through: :studies
   has_many :posts
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :difficulty, presence: true
+  validates :variation, presence: true
 end
--- spec/requests/openings_spec.rb
+++ spec/requests/openings_spec.rb
@@ -96,6 +96,39 @@ RSpec.describe "Openings", type: :request do
       opening = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
+      expect(opening["name"]).to eq("Queen's Gambit")
+      expect(opening["description"]).to eq("White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.")
+      expect(opening["difficulty"]).to eq("Beginner")
+      expect(opening["variation"]).to eq("1. d4 d5 2. c4")
+    end
+
+    it "returns unauthorized when no jwt is provided (user is not logged in)" do
+      post "/openings.json", params: {
+                               name: "Queen's Gambit",
+                               description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
+                               difficulty: "Beginner",
+                               variation: "1. d4 d5 2. c4",
+                             }
+      opening = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+
+    it "returns unprocessable entity with bad data" do
+      user = User.create!(email: "capablanca@email.com", password: "password", admin: true)
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i, # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+
+      post "/openings.json", params: {}, headers: { "Authorization" => "Bearer #{jwt}" }
+      error = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:unprocessable_entity)
     end
   end
 end
