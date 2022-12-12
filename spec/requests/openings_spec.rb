203a86e0850c95b34df8e84142b1368cf55c3804
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-12-12 14:07:47 -0500

    Adds test status and authorization test for opening delete action

--- spec/requests/openings_spec.rb
+++ spec/requests/openings_spec.rb
@@ -40,30 +40,20 @@ RSpec.describe "Openings", type: :request do
       get "/openings.json"
       openings = JSON.parse(response.body)
 
-      expect(openings.length).to eq(4)
+      expect(openings.length).to eq(5)
     end
   end
 
   describe "GET /openings/:id" do
     it "returns a 200 http status for show action" do
-      opening = Opening.create!(
-        name: "Queen's Gambit",
-        description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
-        difficulty: "Beginner",
-        variation: "1. d4 d5 2. c4",
-      )
+      opening = Opening.first
       get "/openings/#{opening.id}.json"
 
       expect(response).to have_http_status(200)
     end
 
     it "returns an array of a post" do
-      opening = Opening.create!(
-        name: "Queen's Gambit",
-        description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
-        difficulty: "Beginner",
-        variation: "1. d4 d5 2. c4",
-      )
+      opening = Opening.first
 
       get "/openings/#{opening.id}.json"
       opening = JSON.parse(response.body)
@@ -77,7 +67,7 @@ RSpec.describe "Openings", type: :request do
 
   describe "POST /openings" do
     it "returns a hash with appropriate attributes" do
-      user = User.create!(email: "capablanca@email.com", password: "password", admin: true)
+      user = User.first
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -115,7 +105,7 @@ RSpec.describe "Openings", type: :request do
     end
 
     it "returns unprocessable entity with bad data" do
-      user = User.create!(email: "capablanca@email.com", password: "password", admin: true)
+      user = User.first
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -129,6 +119,69 @@ RSpec.describe "Openings", type: :request do
       error = JSON.parse(response.body)
 
       expect(response).to have_http_status(:unprocessable_entity)
+    end
+  end
+
+  describe "PATCH /openings/:id" do
+    it "returns a hash with updated values" do
+      user = User.first
+      opening = Opening.first
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i, # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+
+      patch "/openings/#{opening.id}.json", params: { name: "new name" }, headers: { "Authorization" => "Bearer #{jwt}" }
+      opening = JSON.parse(response.body)
+
+      expect(response).to have_http_status(200)
+      expect(opening["name"]).to eq("new name")
+      expect(opening["description"]).to eq("White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.")
+      expect(opening["difficulty"]).to eq("Beginner")
+      expect(opening["variation"]).to eq("1. d4 d5 2. c4")
+    end
+
+    it "returns unauthorized when no jwt is provided (user is not logged in)" do
+      user = User.first
+      opening = Opening.create!(name: "Queen's Gambit",
+                                description: "White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.",
+                                difficulty: "Beginner",
+                                variation: "1. d4 d5 2. c4")
+      patch "/openings/#{opening.id}.json", params: { name: "new name" }
+
+      error = JSON.parse(response.body)
+
+      expect(response).to have_http_status(:unauthorized)
+    end
+  end
+
+  describe "DELETE /openings/:id" do
+    it "returns a 200 status for delete action" do
+      user = User.first
+      opening = Opening.last
+      jwt = JWT.encode(
+        {
+          user_id: user.id, # the data to encode
+          exp: 24.hours.from_now.to_i, # the expiration time
+        },
+        Rails.application.credentials.fetch(:secret_key_base), # the secret key
+        "HS256" # the encryption algorithm
+      )
+
+      delete "/openings/#{opening.id}.json", headers: { "Authorization" => "Bearer #{jwt}" }
+
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns unauthorized when no jwt is provided (user is not logged in)" do
+      opening = Opening.last
+      delete "/openings/#{opening.id}.json"
+
+      expect(response).to have_http_status(:unauthorized)
     end
   end
 end
