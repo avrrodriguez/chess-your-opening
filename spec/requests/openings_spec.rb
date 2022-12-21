f786aa190f2fb68dbee29d306ef1ea0fde4b81bb
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-12-21 13:33:09 -0500

    Bug fixes in opening requests spec

--- spec/requests/openings_spec.rb
+++ spec/requests/openings_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe "Openings", type: :request do
 
   describe "GET /openings/:id" do
     before do
-      opening = FactoryBot.create(:opening)
+      FactoryBot.create(:opening)
     end
 
     it "returns a 200 http status for show action" do
@@ -51,6 +51,7 @@ RSpec.describe "Openings", type: :request do
     end
 
     it "returns a hash with appropriate attributes" do
+      user = User.first
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -88,6 +89,8 @@ RSpec.describe "Openings", type: :request do
     end
 
     it "returns unprocessable entity with bad data" do
+      user = User.first
+
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -106,11 +109,14 @@ RSpec.describe "Openings", type: :request do
 
   describe "PATCH /openings/:id" do
     before do
-      opening = FactoryBot.create(:opening)
-      user = FactoryBot.create(:user, :admin)
+      FactoryBot.create(:opening)
+      FactoryBot.create(:user, :admin)
     end
 
     it "returns a hash with updated values" do
+      user = User.first
+      opening = Opening.first
+
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
@@ -125,9 +131,9 @@ RSpec.describe "Openings", type: :request do
 
       expect(response).to have_http_status(200)
       expect(opening["name"]).to eq("new name")
-      expect(opening["description"]).to eq("White chooses to play the Queen’s Gambit because it gives him the opportunity to exchange his wing pawn to gain more control of the center. This leads to positions where White can constantly put pressure on his opponent. The Queen's Gambit can force black to either lose control of the center or having to play in a cramped position.")
-      expect(opening["difficulty"]).to eq("Beginner")
-      expect(opening["variation"]).to eq("1. d4 d5 2. c4")
+      expect(opening["description"]).to eq(opening["description"])
+      expect(opening["difficulty"]).to eq(opening["difficulty"])
+      expect(opening["variation"]).to eq(opening["variation"])
     end
 
     it "returns unauthorized when no jwt is provided (user is not logged in)" do
@@ -143,11 +149,14 @@ RSpec.describe "Openings", type: :request do
 
   describe "DELETE /openings/:id" do
     before do
-      opening = FactoryBot.create(:opening)
-      user = FactoryBot.create(:user, :admin)
+      FactoryBot.create(:opening)
+      FactoryBot.create(:user, :admin)
     end
 
     it "returns a 200 status for delete action" do
+      user = User.first
+      opening = Opening.first
+
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
