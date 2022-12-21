6824a35ce1c63812ba084b86cc51d8cf43036b44
Abraham V Rodriguez <pqavrr835@gmail.com>
2022-12-21 14:01:44 -0500

    Replaces study and user create instances with factory bot study, user, and opening instances in studies spec.

--- spec/requests/studies_spec.rb
+++ spec/requests/studies_spec.rb
@@ -3,67 +3,26 @@ require "rails_helper"
 RSpec.describe "Studies", type: :request do
   describe "GET /studies" do
     before do
-      FactoryBot.create(:user, :admin)
+      opening = FactoryBot.create(:opening)
+      opening2 = FactoryBot.create(:opening)
+      user = FactoryBot.create(:user, :admin)
+      user2 = FactoryBot.create(:user, :admin, email: "capablanca@email.com")
+      FactoryBot.create(:study, user_id: user.id, opening_id: opening.id)
+      FactoryBot.create(:study, user_id: user2.id, opening_id: opening.id)
+      FactoryBot.create(:study, user_id: user.id, opening_id: opening2.id)
     end
 
     it "returns an array of public studies" do
-      user1 = User.first
-      user2 = User.second
-
-      Study.create!(
-        user_id: user1.id,
-        opening_id: Opening.first.id,
-        notes: "very good",
-        public: true,
-      )
-
-      Study.create!(
-        user_id: user1.id,
-        opening_id: Opening.all[2].id,
-        notes: "very nice",
-        public: false,
-      )
-
-      Study.create!(
-        user_id: user1.id,
-        opening_id: Opening.all[2].id,
-        notes: "very nice",
-        public: true,
-      )
-
       get "/studies.json"
       studies = JSON.parse(response.body)
 
       expect(response).to have_http_status(200)
-      expect(studies.length).to eq(2)
+      expect(studies.length).to eq(3)
     end
 
     it "return an array of a users and public studies" do
       user = User.first
 
-      opening1 = Opening.all[0]
-      opening3 = Opening.all[2]
-
-      # Study.create!(
-      #   user_id: user.id,
-      #   opening_id: opening1.id,
-      #   notes: "very good",
-      #   public: true,
-      # )
-
-      # Study.create!(
-      #   user_id: user.id,
-      #   opening_id: opening3.id,
-      #   notes: "very nice",
-      #   public: false,
-      # )
-
-      # Study.create!(
-      #   user_id: user.id,
-      #   opening_id: opening3.id,
-      #   notes: "very nice",
-      #   public: true,
-      # )
       jwt = JWT.encode(
         {
           user_id: user.id, # the data to encode
